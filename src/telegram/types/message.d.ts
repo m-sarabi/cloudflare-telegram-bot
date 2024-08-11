b8bb2d4c1aa3534075767f65535be720c9bf7a33
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-11 11:10:12 +0330

    Added MessageId, InaccessibleMessage and MaybeInaccessibleMessage types

--- src/telegram/types/message.d.ts
+++ src/telegram/types/message.d.ts
@@ -371,4 +371,43 @@ declare namespace Telegram {
          */
         reply_markup?: Telegram.InlineKeyboardMarkup;
     }
+
+    /**
+     * This object represents a unique message identifier.
+     *
+     * [MessageId - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#messageid)
+     */
+    interface MessageId {
+        /**
+         * Unique message identifier
+         */
+        message_id: number;
+    }
+
+    /**
+     * This object describes a message that was deleted or is otherwise inaccessible to the bot.
+     *
+     * [InaccessibleMessage - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#inaccessiblemessage)
+     */
+    interface InaccessibleMessage {
+        /**
+         * Chat the message belonged to
+         */
+        chat: Telegram.Chat;
+        /**
+         * Unique message identifier inside the chat
+         */
+        message_id: number;
+        /**
+         * Always 0. The field can be used to differentiate regular and inaccessible messages.
+         */
+        date: 0;
+    }
+
+    /**
+     * This object describes a message that can be inaccessible to the bot. It can be one of
+     *
+     * [MaybeInaccessibleMessage - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#maybeinaccessiblemessage)
+     */
+    type MaybeInaccessibleMessage = Telegram.Message | Telegram.InaccessibleMessage;
 }
