6c6347158c1f55758bcfe949412809b66b4ced3a
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-12 21:39:37 +0330

    Added `File` type

--- src/telegram/types/media.d.ts
+++ src/telegram/types/media.d.ts
@@ -276,4 +276,33 @@ declare namespace Telegram {
          */
         file_size?: number;
     }
+
+    /**
+     * This object represents a file ready to be downloaded.
+     * The file can be downloaded via the link `https://api.telegram.org/file/bot<token>/<file_path>`.
+     * It is guaranteed that the link will be valid for at least 1 hour.
+     * When the link expires, a new one can be requested by calling [getFile](https://core.telegram.org/bots/api#getfile).
+     *
+     * > The maximum file size to download is 20 MB
+     *
+     * [File - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#file)
+     */
+    interface File {
+        /**
+         * Identifier for this file, which can be used to download or reuse the file
+         */
+        file_id: string;
+        /**
+         * Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+         */
+        file_unique_id: string;
+        /**
+         * _Optional_. File size in bytes. It can be bigger than 2^31 and some programming languages may have difficulty/silent defects in interpreting it. But it has at most 52 significant bits, so a signed 64-bit integer or double-precision float type are safe for storing this value.
+         */
+        file_size?: number;
+        /**
+         * _Optional_. File path. Use `https://api.telegram.org/file/bot<token>/<file_path>` to get the file.
+         */
+        file_path?: string;
+    }
 }
