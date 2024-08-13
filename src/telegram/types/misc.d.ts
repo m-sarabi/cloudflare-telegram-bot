3eaafe27b8c26df16d01d58da6d835ec4265ef7d
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-13 18:13:02 +0330

    Added `ResponseParameters` type

--- src/telegram/types/misc.d.ts
+++ src/telegram/types/misc.d.ts
@@ -131,4 +131,20 @@ declare namespace Telegram {
          */
         distance: number;
     }
+
+    /**
+     * Describes why a request was unsuccessful.
+     *
+     * [ResponseParameters - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#responseparameters)
+     */
+    interface ResponseParameters {
+        /**
+         * _Optional_. The group has been migrated to a supergroup with the specified identifier.
+         */
+        migrate_to_chat_id?: number;
+        /**
+         * _Optional_. In case of exceeding flood control, the number of seconds left to wait before the request can be repeated
+         */
+        retry_after?: number;
+    }
 }
