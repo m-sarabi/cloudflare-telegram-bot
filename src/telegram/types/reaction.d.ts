af9f281965f9612d52da64bd51ab82c9be6b18c3
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-13 12:10:57 +0330

    Added reaction related types

---
+++ src/telegram/types/reaction.d.ts
@@ -0,0 +1,118 @@
+declare namespace Telegram {
+    /**
+     * This object describes the type of a reaction. Currently, it can be one of
+     *
+     * [ReactionType - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#reactiontype)
+     */
+    type ReactionType =
+        ReactionTypeEmoji
+        | ReactionTypeCustomEmoji;
+
+    /**
+     * The reaction is based on an emoji.
+     *
+     * [ReactionTypeEmoji - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#reactiontypeemoji)
+     */
+    interface ReactionTypeEmoji {
+        /**
+         * Type of the reaction, always “emoji”
+         */
+        type: 'emoji';
+        /**
+         * Reaction emoji.
+         */
+        emoji: string;
+    }
+
+    /**
+     * The reaction is based on a custom emoji.
+     *
+     * [ReactionTypeCustomEmoji - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#reactiontypecustomemoji)
+     */
+    interface ReactionTypeCustomEmoji {
+        /**
+         * Type of the reaction, always “custom_emoji”
+         */
+        type: 'custom_emoji';
+        /**
+         * Custom emoji identifier
+         */
+        custom_emoji_id: string;
+    }
+
+    /**
+     * Represents a reaction added to a message along with the number of times it was added.
+     *
+     * [ReactionCount - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#reactioncount)
+     */
+    interface ReactionCount {
+        /**
+         * Type of the reaction
+         */
+        type: Telegram.ReactionType;
+        /**
+         * Number of times the reaction was added
+         */
+        total_count: number;
+    }
+
+    /**
+     * This object represents a change of a reaction on a message performed by a user.
+     *
+     * [MessageReactionUpdated - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#messagereactionupdated)
+     */
+    interface MessageReactionUpdated {
+        /**
+         * The chat containing the message the user reacted to
+         */
+        chat: Telegram.Chat;
+        /**
+         * Unique identifier of the message inside the chat
+         */
+        message_id: number;
+        /**
+         * _Optional_. The user that changed the reaction, if the user isn't anonymous
+         */
+        user?: Telegram.User;
+        /**
+         * _Optional_. The chat on behalf of which the reaction was changed, if the user is anonymous
+         */
+        actor_chat?: Telegram.Chat;
+        /**
+         * Date of the change in Unix time
+         */
+        date: number;
+        /**
+         * Previous list of reaction types that were set by the user
+         */
+        old_reaction: Telegram.ReactionType[];
+        /**
+         * New list of reaction types that have been set by the user
+         */
+        new_reaction: Telegram.ReactionType[];
+    }
+
+    /**
+     * This object represents reaction changes on a message with anonymous reactions.
+     *
+     * [MessageReactionCountUpdated - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#messagereactioncountupdated)
+     */
+    interface MessageReactionCountUpdated {
+        /**
+         * The chat containing the message
+         */
+        chat: Telegram.Chat;
+        /**
+         * Unique message identifier inside the chat
+         */
+        message_id: number;
+        /**
+         * Date of the change in Unix time
+         */
+        date: number;
+        /**
+         * List of reactions that are present on the message
+         */
+        reactions: Telegram.ReactionCount[];
+    }
+}
