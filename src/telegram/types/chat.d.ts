b0a51de16d7df6ed0eb9f883852dcd17260d269a
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-13 10:05:55 +0330

    Added a bunch of chat member related types

--- src/telegram/types/chat.d.ts
+++ src/telegram/types/chat.d.ts
@@ -316,4 +316,544 @@ declare namespace Telegram {
          */
         users: Telegram.User[];
     }
+
+    /**
+     * This object represents a chat photo.
+     *
+     * [ChatPhoto - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatphoto)
+     */
+    interface ChatPhoto {
+        /**
+         * File identifier of small (160x160) chat photo.
+         * This file_id can be used only for photo download and only for as long as the photo is not changed.
+         */
+        small_file_id: string;
+        /**
+         * Unique file identifier of small (160x160) chat photo, which is supposed to be the same over time and for different bots.
+         * Can't be used to download or reuse the file.
+         */
+        small_file_unique_id: string;
+        /**
+         * File identifier of big (640x640) chat photo.
+         * This file_id can be used only for photo download and only for as long as the photo is not changed.
+         */
+        big_file_id: string;
+        /**
+         * Unique file identifier of big (640x640) chat photo, which is supposed to be the same over time and for different bots.
+         * Can't be used to download or reuse the file.
+         */
+        big_file_unique_id: string;
+    }
+
+    /**
+     * Represents an invite link for a chat.
+     *
+     * [ChatInviteLink - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatinvitelink)
+     */
+    interface ChatInviteLink {
+        /**
+         * The invite link. If the link was created by another chat administrator, then the second part of the link will be replaced with “…”.
+         */
+        invite_link: string;
+        /**
+         * Creator of the link
+         */
+        creator: Telegram.User;
+        /**
+         * True, if users joining the chat via the link need to be approved by chat administrators
+         */
+        creates_join_request: boolean;
+        /**
+         * True, if the link is primary
+         */
+        is_primary: boolean;
+        /**
+         * True, if the link is revoked
+         */
+        is_revoked: boolean;
+        /**
+         * _Optional_. Invite link name
+         */
+        name?: string;
+        /**
+         * _Optional_. Point in time (Unix timestamp) when the link will expire or has been expired
+         */
+        expire_date?: number;
+        /**
+         * _Optional_. The maximum number of users that can be members of the chat simultaneously after joining the chat via this invite link; 1-99999
+         */
+        member_limit?: number;
+        /**
+         * _Optional_. Number of pending join requests created using this link
+         */
+        pending_join_request_count?: number;
+    }
+
+    /**
+     * Represents the rights of an administrator in a chat.
+     *
+     * [ChatAdministratorRights - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatadministratorrights)
+     */
+    interface ChatAdministratorRights {
+        /**
+         * True, if the user's presence in the chat is hidden
+         */
+        is_anonymous: boolean;
+        /**
+         * True, if the administrator can access the chat event log, get boost list, see hidden supergroup and channel members,
+         * report spam messages and ignore slow mode. Implied by any other administrator privilege.
+         */
+        can_manage_chat: boolean;
+        /**
+         * True, if the administrator can delete messages of other users
+         */
+        can_delete_messages: boolean;
+        /**
+         * True, if the administrator can manage video chats
+         */
+        can_manage_video_chats: boolean;
+        /**
+         * True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
+         */
+        can_restrict_members: boolean;
+        /**
+         * True, if the administrator can add new administrators with a subset of their own privileges
+         * or demote administrators that they have promoted, directly or indirectly (promoted by administrators that were appointed by the user)
+         */
+        can_promote_members: boolean;
+        /**
+         * True, if the user is allowed to change the chat title, photo and other settings
+         */
+        can_change_info: boolean;
+        /**
+         * True, if the user is allowed to invite new users to the chat
+         */
+        can_invite_users: boolean;
+        /**
+         * True, if the administrator can post stories to the chat
+         */
+        can_post_stories: boolean;
+        /**
+         * True, if the administrator can edit stories posted by other users, post stories to the chat page,
+         * pin chat stories, and access the chat's story archive
+         */
+        can_edit_stories: boolean;
+        /**
+         * True, if the administrator can delete stories posted by other users
+         */
+        can_delete_stories: boolean;
+        /**
+         * _Optional_. True, if the administrator can post messages in the channel, or access channel statistics; for channels only
+         */
+        can_post_messages?: boolean;
+        /**
+         * _Optional_. True, if the administrator can edit messages of other users and can pin messages; for channels only
+         */
+        can_edit_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to pin messages; for groups and supergroups only
+         */
+        can_pin_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
+         */
+        can_manage_topics?: boolean;
+    }
+
+    /**
+     * This object represents changes in the status of a chat member.
+     *
+     * [ChatMemberUpdated - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberupdated)
+     */
+    interface ChatMemberUpdated {
+        /**
+         * Chat the user belongs to
+         */
+        chat: Telegram.Chat;
+        /**
+         * Performer of the action, which resulted in the change
+         */
+        from: Telegram.User;
+        /**
+         * Date the change was done in Unix time
+         */
+        date: number;
+        /**
+         * Previous information about the chat member
+         */
+        old_chat_member: Telegram.ChatMember;
+        /**
+         * New information about the chat member
+         */
+        new_chat_member: Telegram.ChatMember;
+        /**
+         * _Optional_. Chat invite link, which was used by the user to join the chat; for joining by invite link events only.
+         */
+        invite_link?: Telegram.ChatInviteLink;
+        /**
+         * _Optional_. True, if the user joined the chat after sending a direct join request
+         * without using an invite link and being approved by an administrator
+         */
+        via_join_request?: boolean;
+        /**
+         * _Optional_. True, if the user joined the chat via a chat folder invite link
+         */
+        via_chat_folder_invite_link?: boolean;
+    }
+
+    /**
+     * This object contains information about one member of a chat.
+     *
+     * [ChatMember - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmember)
+     */
+    type ChatMember =
+        ChatMemberOwner
+        | ChatMemberAdministrator
+        | ChatMemberMember
+        | ChatMemberRestricted
+        | ChatMemberLeft
+        | ChatMemberBanned;
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that owns the chat and has all administrator privileges.
+     *
+     * [ChatMemberOwner - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberowner)
+     */
+    interface ChatMemberOwner {
+        /**
+         * The member's status in the chat, always “creator”
+         */
+        status: 'creator';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+        /**
+         * True, if the user's presence in the chat is hidden
+         */
+        is_anonymous: boolean;
+        /**
+         * _Optional_. Custom title for this user
+         */
+        custom_title?: string;
+    }
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that has some additional privileges.
+     *
+     * [ChatMemberAdministrator - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberadministrator)
+     */
+    interface ChatMemberAdministrator {
+        /**
+         * The member's status in the chat, always “administrator”
+         */
+        status: 'administrator';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+        /**
+         * True, if the bot is allowed to edit administrator privileges of that user
+         */
+        can_be_edited: boolean;
+        /**
+         * True, if the user's presence in the chat is hidden
+         */
+        is_anonymous: boolean;
+        /**
+         * True, if the administrator can access the chat event log, get boost list,
+         * see hidden supergroup and channel members, report spam messages and ignore slow mode. Implied by any other administrator privilege.
+         */
+        can_manage_chat: boolean;
+        /**
+         * True, if the administrator can delete messages of other users
+         */
+        can_delete_messages: boolean;
+        /**
+         * True, if the administrator can manage video chats
+         */
+        can_manage_video_chats: boolean;
+        /**
+         * True, if the administrator can restrict, ban or unban chat members, or access supergroup statistics
+         */
+        can_restrict_members: boolean;
+        /**
+         * True, if the administrator can add new administrators with a subset of their own privileges or demote administrators
+         * that they have promoted, directly or indirectly (promoted by administrators that were appointed by the user)
+         */
+        can_promote_members: boolean;
+        /**
+         * True, if the user is allowed to change the chat title, photo and other settings
+         */
+        can_change_info: boolean;
+        /**
+         * True, if the user is allowed to invite new users to the chat
+         */
+        can_invite_users: boolean;
+        /**
+         * True, if the administrator can post stories to the chat
+         */
+        can_post_stories: boolean;
+        /**
+         * True, if the administrator can edit stories posted by other users, post stories to the chat page, pin chat stories,
+         * and access the chat's story archive
+         */
+        can_edit_stories: boolean;
+        /**
+         * True, if the administrator can delete stories posted by other users
+         */
+        can_delete_stories: boolean;
+        /**
+         * _Optional_. True, if the administrator can post messages in the channel, or access channel statistics; for channels only
+         */
+        can_post_messages?: boolean;
+        /**
+         * _Optional_. True, if the administrator can edit messages of other users and can pin messages; for channels only
+         */
+        can_edit_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to pin messages; for groups and supergroups only
+         */
+        can_pin_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to create, rename, close, and reopen forum topics; for supergroups only
+         */
+        can_manage_topics?: boolean;
+        /**
+         * _Optional_. Custom title for this user
+         */
+        custom_title?: string;
+    }
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that has no additional privileges or restrictions.
+     *
+     * [ChatMemberMember - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmembermember)
+     */
+    interface ChatMemberMember {
+        /**
+         * The member's status in the chat, always “member”
+         */
+        status: 'member';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+    }
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that is under certain restrictions in the chat. Supergroups only.
+     *
+     * [ChatMemberRestricted - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberrestricted)
+     */
+    interface ChatMemberRestricted {
+        /**
+         * The member's status in the chat, always “restricted”
+         */
+        status: 'restricted';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+        /**
+         * True, if the user is a member of the chat at the moment of the request
+         */
+        is_member: boolean;
+        /**
+         * True, if the user is allowed to send text messages, contacts, giveaways, giveaway winners, invoices, locations and venues
+         */
+        can_send_messages: boolean;
+        /**
+         * True, if the user is allowed to send audios
+         */
+        can_send_audios: boolean;
+        /**
+         * True, if the user is allowed to send documents
+         */
+        can_send_documents: boolean;
+        /**
+         * True, if the user is allowed to send photos
+         */
+        can_send_photos: boolean;
+        /**
+         * True, if the user is allowed to send videos
+         */
+        can_send_videos: boolean;
+        /**
+         * True, if the user is allowed to send video notes
+         */
+        can_send_video_notes: boolean;
+        /**
+         * True, if the user is allowed to send voice notes
+         */
+        can_send_voice_notes: boolean;
+        /**
+         * True, if the user is allowed to send polls
+         */
+        can_send_polls: boolean;
+        /**
+         * True, if the user is allowed to send animations, games, stickers and use inline bots
+         */
+        can_send_other_messages: boolean;
+        /**
+         * True, if the user is allowed to add web page previews to their messages
+         */
+        can_add_web_page_previews: boolean;
+        /**
+         * True, if the user is allowed to change the chat title, photo and other settings
+         */
+        can_change_info: boolean;
+        /**
+         * True, if the user is allowed to invite new users to the chat
+         */
+        can_invite_users: boolean;
+        /**
+         * True, if the user is allowed to pin messages
+         */
+        can_pin_messages: boolean;
+        /**
+         * True, if the user is allowed to create forum topics
+         */
+        can_manage_topics: boolean;
+        /**
+         * Date when restrictions will be lifted for this user; Unix time. If 0, then the user is restricted forever
+         */
+        until_date: number;
+    }
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that isn't currently a member of the chat,
+     * but may join it themselves.
+     *
+     * [ChatMemberLeft - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberleft)
+     */
+    interface ChatMemberLeft {
+        /**
+         * The member's status in the chat, always “left”
+         */
+        status: 'left';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+    }
+
+    /**
+     * Represents a [chat member](https://core.telegram.org/bots/api#chatmember) that was banned in the chat
+     * and can't return to the chat or view chat messages.
+     *
+     * [ChatMemberBanned - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatmemberbanned)
+     */
+    interface ChatMemberBanned {
+        /**
+         * The member's status in the chat, always “kicked”
+         */
+        status: 'kicked';
+        /**
+         * Information about the user
+         */
+        user: Telegram.User;
+        /**
+         * Date when restrictions will be lifted for this user; Unix time. If 0, then the user is banned forever
+         */
+        until_date: number;
+    }
+
+    /**
+     * Represents a join request sent to a chat.
+     *
+     * [ChatJoinRequest - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatjoinrequest)
+     */
+    interface ChatJoinRequest {
+        /**
+         * Chat to which the request was sent
+         */
+        chat: Telegram.Chat;
+        /**
+         * User that sent the join request
+         */
+        from: Telegram.User;
+        /**
+         * Identifier of a private chat with the user who sent the join request.
+         * The bot can use this identifier for 5 minutes to send messages until the join request is processed,
+         * assuming no other administrator contacted the user.
+         */
+        user_chat_id: number;
+        /**
+         * Date the request was sent in Unix time
+         */
+        date: number;
+        /**
+         * _Optional_. Bio of the user.
+         */
+        bio?: string;
+        /**
+         * _Optional_. Chat invite link that was used by the user to send the join request
+         */
+        invite_link?: Telegram.ChatInviteLink;
+    }
+
+    /**
+     * Describes actions that a non-administrator user is allowed to take in a chat.
+     *
+     * [ChatPermissions - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#chatpermissions)
+     */
+    interface ChatPermissions {
+        /**
+         * _Optional_. True, if the user is allowed to send text messages, contacts, giveaways, giveaway winners, invoices, locations and venues
+         */
+        can_send_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send audios
+         */
+        can_send_audios?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send documents
+         */
+        can_send_documents?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send photos
+         */
+        can_send_photos?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send videos
+         */
+        can_send_videos?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send video notes
+         */
+        can_send_video_notes?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send voice notes
+         */
+        can_send_voice_notes?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send polls
+         */
+        can_send_polls?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to send animations, games, stickers and use inline bots
+         */
+        can_send_other_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to add web page previews to their messages
+         */
+        can_add_web_page_previews?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to change the chat title, photo and other settings. Ignored in public supergroups
+         */
+        can_change_info?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to invite new users to the chat
+         */
+        can_invite_users?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to pin messages. Ignored in public supergroups
+         */
+        can_pin_messages?: boolean;
+        /**
+         * _Optional_. True, if the user is allowed to create forum topics. If omitted defaults to the value of can_pin_messages
+         */
+        can_manage_topics?: boolean;
+    }
 }
