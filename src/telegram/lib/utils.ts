f2b885027123940f419ae3f3f74b1fb0c7a4eb0a
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-14 00:13:01 +0330

    optional params, exporting response type

--- src/telegram/lib/utils.ts
+++ src/telegram/lib/utils.ts
@@ -1,6 +1,6 @@
 import { getEnv } from '../../envManagar';
 
-interface response {
+export interface response {
     ok: boolean,
     result: any,
     description?: string
@@ -13,7 +13,7 @@ export function apiUrl(methodName: string, params?: Record<string, any>) {
     return `https://api.telegram.org/bot${token}/${methodName}${query}`;
 }
 
-export async function callApi(methodName: string, params: Record<string, any>) {
+export async function callApi(methodName: string, params?: Record<string, any>) {
     const response: response = await (await fetch(apiUrl(methodName, params))).json();
     if (!response.ok) {
 
