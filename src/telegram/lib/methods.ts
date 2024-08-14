60dd936dcd7b3b22e20a83260f8e31678f60a9e8
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-14 20:37:16 +0330

    added `banChatMember` method

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -1301,4 +1301,36 @@ export namespace tg {
             file_id
         });
     }
+
+    /**
+     * Use this method to ban a user in a group, a supergroup or a channel. In the case of supergroups and channels,
+     * the user will not be able to return to the chat on their own using invite links, etc.,
+     * unless [unbanned](https://core.telegram.org/bots/api#unbanchatmember) first.
+     * The bot must be an administrator in the chat for this to work and must have the appropriate administrator rights.
+     *
+     * [banChatMember - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#banchatmember)
+     *
+     * @param chat_id >- Unique identifier for the target group or username of the target supergroup or channel (in the format `@channelusername`)
+     * @param user_id >- Unique identifier of the target user
+     * @param [until_date] >- Date when the user will be unbanned; Unix time.
+     * If user is banned for more than 366 days or less than 30 seconds from the current time they are considered to be banned forever.
+     * Applied for supergroups and channels only.
+     * @param [revoke_messages] >- Pass True to delete all messages from the chat for the user that is being removed.
+     * If False, the user will be able to see messages in the group that were sent before the user was removed.
+     * Always True for supergroups and channels.
+     * @returns >- Returns True on success.
+     */
+    export async function banChatMember(
+        chat_id: number | string,
+        user_id: number,
+        until_date?: number,
+        revoke_messages?: boolean,
+    ): Promise<boolean> {
+        return await callApi('banChatMember', {
+            chat_id,
+            user_id,
+            until_date,
+            revoke_messages,
+        });
+    }
 }
