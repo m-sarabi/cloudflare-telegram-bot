dbcc216c3fa236ec31afd088eb198fe8a046666e
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-14 19:14:14 +0330

    added `sendPoll` method

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -356,7 +356,8 @@ export namespace tg {
      * Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file,
      * so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
      * [More information on Sending Files »](https://core.telegram.org/bots/api#sending-files)
-     * @param [disable_notification] >- Sends the message [silently](https://telegram.org/blog/channels-2-0#silent-messages). Users will receive a notification with no sound.
+     * @param [disable_notification] >- Sends the message [silently](https://telegram.org/blog/channels-2-0#silent-messages).
+     * Users will receive a notification with no sound.
      * @param [protect_content] >- Protects the contents of the sent message from forwarding and saving
      * @param [message_effect_id] >- Unique identifier of the message effect to be added to the message; for private chats only
      * @param [reply_parameters] >- Description of the message to reply to
@@ -1041,6 +1042,97 @@ export namespace tg {
             message_thread_id,
             last_name,
             vcard,
+            disable_notification,
+            protect_content,
+            message_effect_id,
+            reply_parameters: JSON.stringify(reply_parameters),
+            reply_markup: JSON.stringify(reply_markup)
+        });
+    }
+
+    /**
+     * Use this method to send a native poll.
+     *
+     * [sendPoll - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#sendpoll)
+     *
+     * @param chat_id >- Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
+     * @param question >- Poll question, 1-300 characters
+     * @param options >- A JSON-serialized list of 2-10 answer options
+     * @param [business_connection_id] >- Unique identifier of the business connection on behalf of which the message will be sent
+     * @param [message_thread_id] >- Unique identifier for the target message thread (topic) of the forum; for forum supergroups only
+     * @param [question_parse_mode] >- Mode for parsing entities in the question.
+     * See [formatting options](https://core.telegram.org/bots/api#formatting-options) for more details.
+     * Currently, only custom emoji entities are allowed
+     * @param [question_entities] >- A JSON-serialized list of special entities that appear in the poll question.
+     * It can be specified instead of question_parse_mode
+     * @param [is_anonymous] >- True, if the poll needs to be anonymous, defaults to True
+     * @param [type] >- Poll type, “quiz” or “regular”, defaults to “regular”
+     * @param [allows_multiple_answers] >- True, if the poll allows multiple answers, ignored for polls in quiz mode, defaults to False
+     * @param [correct_option_id] >- 0-based identifier of the correct answer option, required for polls in quiz mode
+     * @param [explanation] >- Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll,
+     * 0-200 characters with at most 2 line feeds after entities parsing
+     * @param [explanation_parse_mode] >- Mode for parsing entities in the explanation.
+     * See [formatting options](https://core.telegram.org/bots/api#formatting-options) for more details.
+     * @param [explanation_entities] >- A JSON-serialized list of special entities that appear in the poll explanation.
+     * It can be specified instead of explanation_parse_mode
+     * @param [open_period] >- Amount of time in seconds the poll will be active after creation, 5-600. Can't be used together with close_date.
+     * @param [close_date] >- Point in time (Unix timestamp) when the poll will be automatically closed.
+     * Must be at least 5 and no more than 600 seconds in the future. Can't be used together with open_period.
+     * @param [is_closed] >- Pass True if the poll needs to be immediately closed. This can be useful for poll preview.
+     * @param [disable_notification] >- Sends the message [silently](https://telegram.org/blog/channels-2-0#silent-messages).
+     * Users will receive a notification with no sound.
+     * @param [protect_content] >- Protects the contents of the sent message from forwarding and saving
+     * @param [message_effect_id] >- Unique identifier of the message effect to be added to the message; for private chats only
+     * @param [reply_parameters] >- Description of the message to reply to
+     * @param [reply_markup]
+     * >- Additional interface options. A JSON-serialized object for an
+     * >  - [inline keyboard](https://core.telegram.org/bots/features#inline-keyboards),
+     * >  - [custom reply keyboard](https://core.telegram.org/bots/features#keyboards),
+     * >  - instructions to remove a reply keyboard or to force a reply from the user
+     * @returns >-  On success, the sent [Message](https://core.telegram.org/bots/api#message) is returned.
+     */
+    export async function sendPoll(
+        chat_id: number | string,
+        question: string,
+        options: tgTypes.InputPollOption[],
+        business_connection_id?: string,
+        message_thread_id?: number,
+        question_parse_mode?: string,
+        question_entities?: tgTypes.MessageEntity[],
+        is_anonymous?: boolean,
+        type?: string,
+        allows_multiple_answers?: boolean,
+        correct_option_id?: number,
+        explanation?: string,
+        explanation_parse_mode?: string,
+        explanation_entities?: tgTypes.MessageEntity[],
+        open_period?: number,
+        close_date?: number,
+        is_closed?: boolean,
+        disable_notification?: boolean,
+        protect_content?: boolean,
+        message_effect_id?: string,
+        reply_parameters?: tgTypes.ReplyParameters,
+        reply_markup?: tgTypes.InlineKeyboardMarkup | tgTypes.ReplyKeyboardMarkup | tgTypes.ReplyKeyboardRemove | tgTypes.ForceReply
+    ): Promise<tgTypes.Message> {
+        return await callApi('sendPoll', {
+            chat_id,
+            question,
+            options: JSON.stringify(options),
+            business_connection_id,
+            message_thread_id,
+            question_parse_mode,
+            question_entities: JSON.stringify(question_entities),
+            is_anonymous,
+            type,
+            allows_multiple_answers,
+            correct_option_id,
+            explanation,
+            explanation_parse_mode,
+            explanation_entities: JSON.stringify(explanation_entities),
+            open_period,
+            close_date,
+            is_closed,
             disable_notification,
             protect_content,
             message_effect_id,
