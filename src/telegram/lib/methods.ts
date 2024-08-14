d9a219ff8d86d0540b9cc6b3a1346305bb570687
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-14 20:39:33 +0330

    added `unbanChatMember` method

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -1333,4 +1333,31 @@ export namespace tg {
             revoke_messages,
         });
     }
+
+    /**
+     * Use this method to unban a previously banned user in a supergroup or channel.
+     * The user will not return to the group or channel automatically, but will be able to join via link, etc.
+     * The bot must be an administrator for this to work.
+     * By default, this method guarantees that after the call the user is not a member of the chat, but will be able to join it.
+     * So if the user is a member of the chat they will also be removed from the chat.
+     * If you don't want this, use the parameter only_if_banned.
+     *
+     * [unbanChatMember - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#unbanchatmember)
+     *
+     * @param chat_id >- Unique identifier for the target group or username of the target supergroup or channel (in the format `@channelusername`)
+     * @param user_id >- Unique identifier of the target user
+     * @param [only_if_banned] >- Do nothing if the user is not banned
+     * @returns >- Returns True on success.
+     */
+    export async function unbanChatMember(
+        chat_id: number | string,
+        user_id: number,
+        only_if_banned?: boolean,
+    ): Promise<boolean> {
+        return await callApi('unbanChatMember', {
+            chat_id,
+            user_id,
+            only_if_banned,
+        });
+    }
 }
