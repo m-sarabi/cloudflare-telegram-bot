ac7ff4b385ab4c9dc418f30cbbe58d7dcad98e2d
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-15 22:30:12 +0330

    added stickers and inline query to global methods

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -1,9 +1,13 @@
 import * as availableMethods from './available_methods';
 import * as gettingUpdates from './getting_updates';
 import * as updatingMessages from './updating_messages';
+import * as stickers from './stickers';
+import * as inlineQuery from './inline_query';
 
 export const tg = {
     ...availableMethods.tg,
     ...gettingUpdates.tg,
     ...updatingMessages.tg,
+    ...stickers.tg,
+    ...inlineQuery.tg,
 };
