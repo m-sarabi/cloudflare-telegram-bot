74c9dc750a1ef000aaa4c00fd1d366c278666c55
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-16 00:24:57 +0330

    added payments, passport and games to global methods

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -3,6 +3,9 @@ import * as gettingUpdates from './getting_updates';
 import * as updatingMessages from './updating_messages';
 import * as stickers from './stickers';
 import * as inlineQuery from './inline_query';
+import * as payments from './payments';
+import * as passport from './passport';
+import * as games from './games';
 
 export const tg = {
     ...availableMethods.tg,
@@ -10,4 +13,7 @@ export const tg = {
     ...updatingMessages.tg,
     ...stickers.tg,
     ...inlineQuery.tg,
+    ...payments.tg,
+    ...passport.tg,
+    ...games.tg,
 };
