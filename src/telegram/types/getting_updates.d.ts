7b7bda081d9b4dc2ebc24baccc277b4f04ea197e
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-15 17:27:08 +0330

    added `getting updates` types in a separate file

--- src/telegram/types/getting_updates.d.ts
+++ src/telegram/types/getting_updates.d.ts
@@ -110,4 +110,48 @@ declare namespace tgTypes {
          */
         removed_chat_boost?: tgTypes.ChatBoostRemoved;
     }
+
+    /**
+     * Describes the current status of a webhook.
+     *
+     * [WebhookInfo - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#webhookinfo)
+     */
+    interface WebhookInfo {
+        /**
+         * Webhook URL, may be empty if webhook is not set up
+         */
+        url: string;
+        /**
+         * True, if a custom certificate was provided for webhook certificate checks
+         */
+        has_custom_certificate: boolean;
+        /**
+         * Number of updates awaiting delivery
+         */
+        pending_update_count: number;
+        /**
+         * _Optional_. Currently used webhook IP address
+         */
+        ip_address?: string;
+        /**
+         * _Optional_. Unix time for the most recent error that happened when trying to deliver an update via webhook
+         */
+        last_error_date?: number;
+        /**
+         * _Optional_. Error message in human-readable format for the most recent error that happened when trying to deliver an update via webhook
+         */
+        last_error_message?: string;
+        /**
+         * _Optional_. Unix time of the most recent error that happened when trying to synchronize available updates with Telegram datacenters
+         */
+        last_synchronization_error_date?: number;
+        /**
+         * _Optional_. The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery
+         */
+        max_connections?: number;
+        /**
+         * _Optional_. A list of update types the bot is subscribed to. Defaults to all update types except chat_member
+         */
+        allowed_updates?: string[];
+    }
 }
