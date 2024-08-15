7a777e847e37c97dc7653c43abd098d64dfeaf29
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-15 15:57:04 +0330

    added `editForumTopic` method

--- src/telegram/lib/methods.ts
+++ src/telegram/lib/methods.ts
@@ -2848,4 +2848,43 @@ export namespace tg {
             icon_custom_emoji_id,
         });
     }
+
+    /**
+     * Use this method to edit name and icon of a topic in a forum supergroup chat.
+     * The bot must be an administrator in the chat for this to work and must have the can_manage_topics administrator rights,
+     * unless it is the creator of the topic.
+     *
+     * [editForumTopic - On Telegram Bot API Documentation](https://core.telegram.org/bots/api#editforumtopic)
+     *
+     * @param chat_id `Required`
+     * >- Unique identifier for the target chat or username of the target supergroup (in the format `@supergroupusername`)
+     * @param message_thread_id `Required`
+     * >- Unique identifier for the target message thread of the forum topic
+     * @param [name] `Optional`
+     * >- New topic name, 0-128 characters. If not specified or empty, the current name of the topic will be kept
+     * @param [icon_custom_emoji_id] `Optional`
+     * >- New unique identifier of the custom emoji shown as the topic icon.
+     * Use [getForumTopicIconStickers](https://core.telegram.org/bots/api#getforumtopiciconstickers)
+     * to get all allowed custom emoji identifiers. Pass an empty string to remove the icon. If not specified, the current icon will be kept
+     * @returns >- true on success.
+     */
+    export async function editForumTopic(
+        {
+            chat_id,
+            message_thread_id,
+            name,
+            icon_custom_emoji_id,
+        }: {
+            chat_id: number | string;
+            message_thread_id: number;
+            name?: string;
+            icon_custom_emoji_id?: string;
+        }): Promise<boolean> {
+        return await callApi('editForumTopic', {
+            chat_id,
+            message_thread_id,
+            name,
+            icon_custom_emoji_id,
+        });
+    }
 }
