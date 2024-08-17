ecf32a8b54882aa9565eeda6e86c0ce9eafafc10
m-sarabi <m.sarabi.jkd@gmail.com>
2024-08-17 04:42:16 +0330

    added placeholders for every update type

--- src/telegram/handlers/handleUpdates.ts
+++ src/telegram/handlers/handleUpdates.ts
@@ -1,4 +1,6 @@
 import { getEnv } from '../utils/envManager';
+import { handleMessage } from './handleMessage';
+import { handleCallbackQuery } from './handleCallbackQuery';
 
 export async function handleWebhook(request: Request): Promise<Response> {
     if (request.headers.get('X-Telegram-Bot-Api-Secret-Token') !== getEnv().SECRET) {
@@ -12,9 +14,30 @@ export async function handleWebhook(request: Request): Promise<Response> {
 
 async function onUpdate(update: tgTypes.Update) {
     if (update.message) {
-        // handle messages
+        await handleMessage(update.message);
     } else if (update.edited_message) {
-        // handle edited messages
+    } else if (update.channel_post) {
+    } else if (update.edited_channel_post) {
+    } else if (update.business_connection) {
+    } else if (update.business_message) {
+    } else if (update.edited_business_message) {
+    } else if (update.deleted_business_messages) {
+    } else if (update.message_reaction) {
+    } else if (update.message_reaction_count) {
+    } else if (update.inline_query) {
+    } else if (update.chosen_inline_result) {
+    } else if (update.callback_query) {
+        await handleCallbackQuery(update.callback_query);
+    } else if (update.shipping_query) {
+    } else if (update.pre_checkout_query) {
+    } else if (update.poll) {
+    } else if (update.poll_answer) {
+    } else if (update.my_chat_member) {
+    } else if (update.chat_member) {
+    } else if (update.chat_join_request) {
+    } else if (update.chat_boost) {
+    } else if (update.removed_chat_boost) {
+    } else {
+        console.log(update);
     }
-    // handle other update types
 }
